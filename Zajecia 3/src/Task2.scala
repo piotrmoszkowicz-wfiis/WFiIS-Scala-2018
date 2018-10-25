e4dc889e9c55cccaf8597a2fc943fc04d7b079a0
Piotr Moszkowicz <piotr@moszkowicz.pl>
2018-10-25 21:50:26 +0200

    Zajecia 3 - fix final tailRecursion

--- Zajecia 3/src/Task2.scala
+++ Zajecia 3/src/Task2.scala
@@ -18,14 +18,14 @@ object Task2 {
     var result = points
     var bestIteration = 0
 
-    val initialDistance = calculateDistanceForAll(points, 10)
+    val initialDistance = calculateDistanceForAll(points, 10, 0.0)
     var bestDistance = initialDistance
 
     for(i <- List.range(1, 100)) {
       val random = scala.util.Random.nextInt(8)+1
       val (pointsPartOne, pointsPartTwo) = points.splitAt(random)
       val newPoints = pointsPartTwo ++ pointsPartOne
-      val newDistance = calculateDistanceForAll(newPoints, 10)
+      val newDistance = calculateDistanceForAll(newPoints, 10, 0.0)
 
       if (newDistance < bestDistance) {
         bestDistance = newDistance
@@ -40,11 +40,12 @@ object Task2 {
     println(bestIteration)
   }
 
-  def calculateDistanceForAll(points: List[(Int, Int)], currentPoint: Int): Double = {
+  @scala.annotation.tailrec def calculateDistanceForAll(points: List[(Int, Int)], currentPoint: Int, distance: Double): Double = {
     if (currentPoint == 1) {
-      return 0.0
+      return distance
     }
-    calculateDistance(points(currentPoint-1), points(currentPoint-2)) + calculateDistanceForAll(points, currentPoint - 1)
+    val newCount = distance + calculateDistance(points(currentPoint-1), points(currentPoint-2))
+    calculateDistanceForAll(points, currentPoint - 1, newCount)
   }
 
   def calculateDistance(firstPoint: (Int, Int), secondPoint: (Int, Int)): Double = {
