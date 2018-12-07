eaba088ea6a9869b4b083ff5ee3f99f7075ff326
Piotr Moszkowicz <piotr@moszkowicz.pl>
2018-12-07 18:14:35 +0100

    Added implicit

--- Prep Zajecia 8/src/Task1_3.scala
+++ Prep Zajecia 8/src/Task1_3.scala
@@ -12,6 +12,11 @@ trait Transform extends MyString {
   def transform(func: String => String): String = func(content)
 }
 
+case class SpacedWord( str: String ) extends MyString ( str ) with Wrap with Transform {
+  wrap("/", "/")
+  transform( _.toUpperCase )
+}
+
 object Task1_3 {
   def main(args: Array[String]): Unit = {
     val s1 = new MyString("Hello") with Wrap wrap(">>", "<<")
@@ -19,5 +24,10 @@ object Task1_3 {
     val s2 = new MyString("There")  with Transform transform(
       _.toLowerCase )
     println(s2)
+
+    implicit def toSpaceWord(str: String): SpacedWord = SpacedWord(str)
+
+    val w = List[SpacedWord]( "you", "manage", "it", "easily" )
+    println( w.mkString("") )
   }
 }
